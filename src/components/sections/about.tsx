7e1b925c6262d99d2a1ecf7e23fd113e18398ab7
hyobin-mo <ahgyqls@gmail.com>
2026-03-17 17:57:38 +0900

    Update about.tsx

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -14,7 +14,7 @@ const skillColors = [
 
 export function AboutSection() {
   return (
-    <section id="about" className="relative py-24 md:py-32" style={{backgroundColor: "#98B1C4"}}>
+    <section id="about" className="relative py-24 md:py-32" style={{backgroundColor: "#C7E0F4"}}>
       <div className="absolute inset-x-0 bottom-0 h-[3px] bg-black" />
 
       <div className="mx-auto max-w-7xl px-6">
